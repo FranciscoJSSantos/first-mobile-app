f1b47c8d75ae82444317df3cc7ffe5301704dbbe
Francisco José <francisco.jsantos@souunit.com.br>
2021-03-12 22:46:47 -0300

    Criação da Tela login

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB663NCuTO0WHA80snxcjcLtiaulKpmsjA",
+    authDomain: "first-mobile-app-9e559.firebaseapp.com",
+    projectId: "first-mobile-app-9e559",
+    storageBucket: "first-mobile-app-9e559.appspot.com",
+    messagingSenderId: "711383547920",
+    appId: "1:711383547920:web:4258edc87d6c1f2d0c4a95",
+    measurementId: "G-V4Z5PEWWVJ"
+  }
 };
 
 /*
